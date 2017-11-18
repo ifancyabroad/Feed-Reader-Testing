a5538250ca2cdf082b8594d44cfb686c02fc2a40
Edgar Nightingale <edgar.nightingale@btinternet.com>
2017-11-18 14:36:24 +0000

    test: Added test for name on each feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -42,6 +42,11 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+		 it('has a name', function() {
+			 allFeeds.forEach(function(feed) {
+				 expect(feed.name).toBeTruthy();
+			 });
+		 });
     });
 
 
